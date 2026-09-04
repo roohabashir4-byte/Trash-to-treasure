c05ea1fc2c06c1204b10898f7bb19786a60893d7
roohabashir4-byte <roohabashir4@gmail.com>
2026-09-04 06:40:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ Condition must be one of: Clean, Mixed, Dirty, Damaged, Unknown.
 """
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+       gemini-3.6-flash
         contents=[image, prompt],
     )
 
